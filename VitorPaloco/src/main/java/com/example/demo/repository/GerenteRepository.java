7f53ffb7b23fa805c749748682f1c5024e46785d
Vitor Paloco <127983254+VitorPaloco@users.noreply.github.com>
2024-03-25 23:15:38 -0300

    Delete VitorPaloco directory

--- VitorPaloco/src/main/java/com/example/demo/repository/GerenteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Gerente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GerenteRepository extends JpaRepository<Gerente, Long> {
-}
