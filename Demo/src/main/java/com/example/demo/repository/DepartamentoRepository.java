030a688e1024afb178d9d56195d134ac2d3634c8
Vitor Paloco <127983254+VitorPaloco@users.noreply.github.com>
2024-04-01 21:00:56 -0300

    Delete Demo directory

--- Demo/src/main/java/com/example/demo/repository/DepartamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Departamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartamentoRepository extends JpaRepository<Departamento,Long> {
-}
--- Demo/src/main/java/com/example/demo/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario,Long> {
-}
